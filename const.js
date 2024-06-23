f7dd810b97b7b500feb0c2f1d1cf4c6ab5b8497b
Yuthala <dinapolousova@gmail.com>
2024-06-23 17:27:12 +0300

    forward message

--- const.js
+++ const.js
@@ -7,8 +7,10 @@ const commands = `
 const btn_1Text = 'Если вы вернули повербанк, но аренда не завершилась, вы будете переключены на оператора'
 const btn_2Text = 'Возможно на карте не хватает средств для оплаты аренды, после оплаты аренды депозит будет полностью возвращен'
 const btn_3Text = 'Стоимость: до 5 минут - бесплатно, до 59 минут - 50₽, до конца суток - 150₽, каждые последующие сутки 200₽ '
+const btn_5Text = 'Отправьте 1, если хотите заказать чеснок. Отправьте 2, если лук'
 
 module.exports.commands = commands
 module.exports.btn_1Text = btn_1Text
 module.exports.btn_2Text = btn_2Text
 module.exports.btn_3Text = btn_3Text
+module.exports.btn_5Text = btn_5Text
--- index.js
+++ index.js
@@ -1,11 +1,15 @@
-const { Telegraf, Markup } = require('telegraf')
-const { message } = require('telegraf/filters')
-require('dotenv').config()
-const text = require('./const')
+const { Telegraf, Markup } = require('telegraf');
+const { message } = require('telegraf/filters');
+require('dotenv').config();
+const text = require('./const');
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
+const bot = new Telegraf(process.env.BOT_TOKEN);
+
+
 bot.start((ctx) => ctx.reply(`Добро пожаловать ${ctx.message.from.first_name ? ctx.message.from.first_name : 'незнакомец'}! Какой вопрос вас интересует?`,getMainMenu()))
 bot.help((ctx) => ctx.reply(text.commands))
+
+// bot.use(ctx => {console.log(ctx.update.message.message_id)})
 // bot.on(message('sticker'), (ctx) => ctx.reply('👍'))
 //bot.hears('hi', (ctx) => ctx.reply('Hey there')) // Проверка текста напечатанного пользователем?
 
@@ -15,13 +19,13 @@ bot.help((ctx) => ctx.reply(text.commands))
         try {
        await ctx.replyWithHTML('<b>Вопросы</b>', Markup.inlineKeyboard(
             [
-                [Markup.button.callback('Аренда не завершилась', 'btn_1')],
+                [Markup.button.callback('Сделать заказ', 'btn_1')],
                 [Markup.button.callback('Депозит не вернулся', 'btn_2')],
                 [Markup.button.callback('Тарифы и условия аренды(Частые вопросы)', 'btn_3')],
                 [Markup.button.callback('Сотрудничество', 'btn_4')],
-                [Markup.button.callback('Аккумулятор не заряжает', 'btn_5')],
-                [Markup.button.callback('Аккумулятор не вышел из слота', 'btn_6')],
-                [Markup.button.callback('Аккумулятор увезен/утерян/украден', 'btn_7')]
+                [Markup.button.callback('Сделать заказ', 'btn_5')],
+                [Markup.button.callback('Аккумулятор не вышел из слота', 'btn_6')]
+                // [Markup.button.callback('Аккумулятор увезен/утерян/украден', 'btn_7')]
             ]
         ))
     
@@ -39,34 +43,70 @@ bot.help((ctx) => ctx.reply(text.commands))
                 [Markup.button.callback('Депозит не вернулся', 'btn_2')],
                 [Markup.button.callback('Тарифы и условия аренды(Частые вопросы)', 'btn_3')],
                 [Markup.button.callback('Сотрудничество', 'btn_4')],
-                [Markup.button.callback('Аккумулятор не заряжает', 'btn_5')],
-                [ Markup.button.callback('Аккумулятор не вышел из слота', 'btn_6')],
-                [Markup.button.callback('Аккумулятор увезен/утерян/украден', 'btn_7')]
+                [Markup.button.callback('Сделать заказ', 'btn_5')],
+                [Markup.button.callback('Аккумулятор не вышел из слота', 'btn_6')]
+                // [Markup.button.callback('Аккумулятор увезен/утерян/украден', 'btn_7')]
             ]
         )
     }
 
+    bot.on("message", async (ctx) => {
+        console.log(`ctx.message ${ctx.message}`);
+        const msgId = ctx.update.message.message_id;
+        // const fromId = ctx.callbackQuery.from.id;
+        const id = '573562828';
 
-function addActionBot(name, src, text) {
-    bot.action(name, async (ctx) => {
+        console.log(`message_id ${msgId}`);
+
         try {
-            await ctx.answerCbQuery()
-            if (src !== false) {
-                await ctx.replyWithPhoto({
-                    source: src
-                })
+            // if(text !== '/start') {
+                await ctx.forwardMessage(id, msgId);
+                // await ctx.bot.sendMessage(chatId, 'Сделан заказ')
+            //} 
+        } catch (e) {
+            console.error(e)
+        }
+    })
+
+function addActionBot(name, text) {
+    bot.action(name, async (ctx) => {
+
+        // console.log(ctx.update.message);
+        //const chatId = ctx.message.chat.id
+        // const text = ctx.message.text
+        const msgId = ctx.update.message.message_id
+        const fromId = ctx.callbackQuery.from.id;
+        const id = '573562828';
+
+        try {
+            if(text !== '/start') {
+                await ctx.forwardMessage(id, msgId, fromId);
+                // await ctx.bot.sendMessage(chatId, 'Сделан заказ')
             }
-          await ctx.replyWithHTML(text, {
-            disable_web_page_preview: true
-          }) 
+
+            // await ctx.answerCbQuery()
+            // if (src !== false) {
+            //     await ctx.replyWithPhoto({
+            //         source: src
+            //     })
+            // }
+        //   await ctx.replyWithHTML(text, {
+        //     disable_web_page_preview: true
+        //   }) 
+
+        //   await ctx.telegram.forwardMessage(id, chatId)
+        //   await ctx.telegram.forwardMessage(whereToSendId, ctx.message.chat.id, ctx.message.message_id).then(function(){
+        //     console.log("mesage forwaded")
+        //     });
         } catch (e) {
             console.error(e)
         }
     })
 }
-addActionBot('btn_1',false, text.btn_1Text)
-addActionBot('btn_2',false, text.btn_2Text)
-addActionBot('btn_3',false, text.btn_3Text)
+// addActionBot('btn_1', text.btn_1Text)
+// // addActionBot('btn_2',false, text.btn_2Text)
+// // addActionBot('btn_3',false, text.btn_3Text)
+// addActionBot('btn_5', text.btn_5Text)
 
 bot.launch()
 
