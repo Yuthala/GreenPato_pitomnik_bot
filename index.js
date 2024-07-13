2091fc205343f05884f3041f5cf0df407275869e
Yuthala <dinapolousova@gmail.com>
2024-07-13 15:40:05 +0300

    copied from Downloads

--- index.js
+++ index.js
@@ -1,103 +1,26 @@
-// // const { Telegraf, Markup } = require('telegraf');
-// // const { message } = require('telegraf/filters');
-// // // Changing max listeners for node
-// // require('events').EventEmitter.defaultMaxListeners = 0;
-require('dotenv').config();
-// // const text = require('./const');
+const { Telegraf, Markup } = require('telegraf')
+//const { message } = require('telegraf/filters')
+require('dotenv').config()
+const text = require('./const')
 
-// // const bot = new Telegraf(process.env.BOT_TOKEN);
-
-
-// // bot.start((ctx) => ctx.reply(`Добро пожаловать ${ctx.message.from.first_name ? ctx.message.from.first_name : 'незнакомец'}! Какой вопрос вас интересует?`,getMainMenu()))
-// // bot.help((ctx) => ctx.reply(text.commands))
-
-
-// // // Если вызывать меню по команде /ask
-// //     bot.command('ask', async (ctx) => {
-// //         try {
-// //        await ctx.replyWithHTML('<b>Вопросы</b>', Markup.inlineKeyboard(
-// //             [
-// //                 [Markup.button.callback('Сделать заказ', 'btn_1')],
-// //                 [Markup.button.callback('Депозит не вернулся', 'btn_2')],
-// //                 [Markup.button.callback('Тарифы и условия аренды(Частые вопросы)', 'btn_3')],
-// //                 [Markup.button.callback('Сотрудничество', 'btn_4')],
-// //                 [Markup.button.callback('Сделать заказ', 'btn_5')],
-// //                 [Markup.button.callback('Аккумулятор не вышел из слота', 'btn_6')]
-// //                 // [Markup.button.callback('Аккумулятор увезен/утерян/украден', 'btn_7')]
-// //             ]
-// //         ))
-    
-// //         } catch(e) {
-// //             console.error(e)
-// //         }
-        
-// //     })
-
-// // // Если вызывать меню по команде /start 
-// //     function getMainMenu() {
-// //         return Markup.inlineKeyboard(
-// //             [
-// //                 [Markup.button.callback('Аренда не завершилась', 'btn_1')],
-// //                 [Markup.button.callback('Депозит не вернулся', 'btn_2')],
-// //                 [Markup.button.callback('Тарифы и условия аренды(Частые вопросы)', 'btn_3')],
-// //                 [Markup.button.callback('Сотрудничество', 'btn_4')],
-// //                 [Markup.button.callback('Сделать заказ', 'btn_5')],
-// //                 [Markup.button.callback('Аккумулятор не вышел из слота', 'btn_6')]
-// //                 // [Markup.button.callback('Аккумулятор увезен/утерян/украден', 'btn_7')]
-// //             ]
-// //         )
-// //     }
-
-// //     bot.on("message", async (ctx) => {
-// //         console.log(`ctx.update ${ctx.update}`);
-// //         const msgId = ctx.update.message.message_id;
-// //         // const fromId = ctx.callbackQuery.from.id;
-// //         const id = '573562828';
-
-// //         console.log(`message_id ${msgId}`);
-
-// //         try {         
-// //                 if(ctx.message.reply_to_message 
-// //                     && ctx.message.reply_to_message.forward_from 
-// //                     && ctx.message.from.id === id) {
-// //                     ctx.telegram.copyMessage(ctx.message.reply_to_message.forward_from.id, ctx.message);
-// //                     console.log(`reply_to ${ctx.message.reply_to_message.forward_from.id}`);
-// //                 } else {
-// //                     await ctx.forwardMessage(id, msgId);
-// //                 }
-// //         } catch (e) {
-// //             console.error(e)
-// //         }
-// //     })
-
-
-
-// bot.launch()
-
-// Подключаем модули
-const Telegraf = require('telegraf');
-// const HttpsProxyAgent = require('https-proxy-agent');
 // Общие настройки
 let config = {
-    "token": process.env.BOT_TOKEN, // Токен бота
+    "token": "YOUR_TOKEN", // Токен бота
     "admin": 573562828 // id владельца бота
 };
-// Создаем объект бота
-const bot = new Telegraf(config.token, {
-        // Если надо ходить через прокси - укажите: user, pass, host, port
-        // telegram: { agent: new HttpsProxyAgent('http://user:pass@host:port') }
-    }
-);
+ 
 // Текстовые настройки
 let replyText = {
     "helloAdmin": "Привет админ, ждем сообщения от пользователей",
     "helloUser":  "Приветствую, отправьте мне сообщение. Постараюсь ответить в ближайшее время.",
     "replyWrong": "Для ответа пользователю используйте функцию Ответить/Reply."
 };
+
 // Проверяем пользователя на права
 let isAdmin = (userId) => {
     return userId == config.admin;
 };
+
 // Перенаправляем админу от пользователя или уведомляем админа об ошибке
 let forwardToAdmin = (ctx) => {
     if (isAdmin(ctx.message.from.id)) {
@@ -106,12 +29,33 @@ let forwardToAdmin = (ctx) => {
         ctx.forwardMessage(config.admin, ctx.from.id, ctx.message.id);
     }
 };
-// Старт бота
-bot.start((ctx) => {
-    ctx.reply(isAdmin(ctx.message.from.id)
-        ? replyText.helloAdmin
-        : replyText.helloUser);
-});
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+bot.start((ctx) => ctx.reply(`Добро пожаловать ${isAdmin(ctx.message.from.id) ? replyText.helloAdmin : ctx.message.from.first_name }! Какой вопрос вас интересует?`,getMainMenu()))
+bot.help((ctx) => ctx.reply(text.commands))
+
+// Если вызывать меню по команде /ask
+    bot.command('ask', async (ctx) => {
+        try {
+       await ctx.replyWithHTML('<b>Вопросы</b>', Markup.inlineKeyboard(
+            [
+                [Markup.button.callback('Аренда не завершилась', 'btn_1')],
+                [Markup.button.callback('Депозит не вернулся', 'btn_2')],
+                [Markup.button.callback('Тарифы и условия аренды(Частые вопросы)', 'btn_3')],
+                [Markup.button.callback('Сотрудничество', 'btn_4')],
+                [Markup.button.callback('Аккумулятор не заряжает', 'btn_5')],
+                [Markup.button.callback('Аккумулятор не вышел из слота', 'btn_6')],
+                [Markup.button.callback('Аккумулятор увезен/утерян/украден', 'btn_7')]
+            ]
+        ))
+    
+        } catch(e) {
+            console.error(e)
+        }
+        
+    })
+
 // Слушаем на наличие объекта message
 bot.on('message', (ctx) => {
     // убеждаемся что это админ ответил на сообщение пользователя
@@ -125,8 +69,45 @@ bot.on('message', (ctx) => {
         forwardToAdmin(ctx);
     }
 });
-// запускаем бот
-bot.launch();
+
+// Если вызывать меню по команде /start 
+    function getMainMenu() {
+        return Markup.inlineKeyboard(
+            [
+                [Markup.button.callback('Аренда не завершилась', 'btn_1')],
+                [Markup.button.callback('Депозит не вернулся', 'btn_2')],
+                [Markup.button.callback('Тарифы и условия аренды(Частые вопросы)', 'btn_3')],
+                [Markup.button.callback('Сотрудничество', 'btn_4')],
+                [Markup.button.callback('Аккумулятор не заряжает', 'btn_5')],
+                [Markup.button.callback('Аккумулятор не вышел из слота', 'btn_6')],
+                [Markup.button.callback('Аккумулятор увезен/утерян/украден', 'btn_7')]
+            ]
+        )
+    }
+
+
+function addActionBot(name, src, text) {
+    bot.action(name, async (ctx) => {
+        try {
+            await ctx.answerCbQuery()
+            if (src !== false) {
+                await ctx.replyWithPhoto({
+                    source: src
+                })
+            }
+          await ctx.replyWithHTML(text, {
+            disable_web_page_preview: true
+          }) 
+        } catch (e) {
+            console.error(e)
+        }
+    })
+}
+addActionBot('btn_1',false, text.btn_1Text)
+addActionBot('btn_2',false, text.btn_2Text)
+addActionBot('btn_3',false, text.btn_3Text)
+
+bot.launch()
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'))
